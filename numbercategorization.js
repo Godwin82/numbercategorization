aa68ba297dc0182d04b876961d47afbe1d452137
Godwin Homadzi <godwinhomadzi820@gmail.com>
2025-02-17 14:22:43 -0800

    categorize number

---
+++ numbercategorization.js
@@ -0,0 +1,15 @@
+let num = 12;
+
+if (num < 0){
+    console.log("the number is negative")
+}
+else if(num === 0){
+    console.log("the number is zero")
+}
+
+else if (num > 0 && num % 2 === 0){
+    console.log("the number is positive and even")
+}
+else{
+   console.log("the number is positive and odd")
+}
